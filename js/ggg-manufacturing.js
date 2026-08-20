42fcffe1a6cf88f174522bdedf3e5e0c2bf3dc89
zachtothefuture <zach@futurenostalgia.io>
2026-08-20 15:47:36 -0700

    Update ggg-manufacturing.js

--- js/ggg-manufacturing.js
+++ js/ggg-manufacturing.js
@@ -1,7 +1,12 @@
 /* ==========================================================
    GGG Manufacturing Engine
+   Pipeline Diagnostic
 ========================================================== */
 
 console.log("👻 GGG Manufacturing Loaded");
 
 document.documentElement.classList.add("ggg-loaded");
+
+const marker = document.createElement("div");
+marker.id = "ggg-pipeline-test";
+document.body.appendChild(marker);
