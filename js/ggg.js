dd1727220d47fc3722a768847553b75ce1384505
zachtothefuture <zach@futurenostalgia.io>
2026-08-20 15:38:35 -0700

    Update ggg.js

--- js/ggg.js
+++ js/ggg.js
@@ -1,6 +1,3 @@
-<link rel="stylesheet"
-      href="https://zachtothefuture.github.io/ggg-public-assets/css/ggg.css?v=1.0">
+/* GGG Manufacturing Engine */
 
-<script defer
-        src="https://zachtothefuture.github.io/ggg-public-assets/js/ggg.js?v=1.0">
-</script>
+console.log("GGG JS loaded.");
