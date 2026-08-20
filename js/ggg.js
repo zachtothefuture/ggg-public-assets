8716fdfea56a86699b5edea8bc752b44a57dd9ea
zachtothefuture <zach@futurenostalgia.io>
2026-08-20 15:37:20 -0700

    Create ggg.js

---
+++ js/ggg.js
@@ -0,0 +1,6 @@
+<link rel="stylesheet"
+      href="https://zachtothefuture.github.io/ggg-public-assets/css/ggg.css?v=1.0">
+
+<script defer
+        src="https://zachtothefuture.github.io/ggg-public-assets/js/ggg.js?v=1.0">
+</script>
